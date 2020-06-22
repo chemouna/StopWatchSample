5c97b951a7c3d30bbb70709a2496c779956bb41d
Mouna Cheikhna <cheikhnamouna@gmail.com>
2020-06-21 22:56:40 -0700

    Step 6: Increase unit tests.

--- app/src/test/java/com/tutorial/stopwatch/MainViewModelTest.kt
+++ app/src/test/java/com/tutorial/stopwatch/MainViewModelTest.kt
@@ -1,6 +1,7 @@
 package com.tutorial.stopwatch
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import junit.framework.Assert.assertEquals
 import junit.framework.Assert.assertFalse
 import junit.framework.Assert.assertTrue
 import org.junit.Before
@@ -25,7 +26,7 @@ class MainViewModelTest {
     fun `toggle running timer stops timer`() {
         // timer starts as not running
         // start timer
-        mainViewModel.toggleTimer(0)
+        mainViewModel.toggleTimer(10)
 
         assertTrue(mainViewModel.timerState().value!!)
     }
@@ -33,10 +34,36 @@ class MainViewModelTest {
     @Test
     fun `toggle stopped timer starts timer`() {
         // start timer
-        mainViewModel.toggleTimer(0)
+        mainViewModel.toggleTimer(12)
         // stop timer
-        mainViewModel.toggleTimer(0)
+        mainViewModel.toggleTimer(25)
 
         assertFalse(mainViewModel.timerState().value!!)
     }
+
+    @Test
+    fun `reset timer stops timer`() {
+        // start timer
+        mainViewModel.toggleTimer(12)
+
+        val elapsedTime: Long = 25
+        mainViewModel.resetTimer(elapsedTime)
+
+        // stops timer
+        assertFalse(mainViewModel.timerState().value!!)
+        // sets base to correct value
+        assertEquals(elapsedTime, mainViewModel.timerBase().value)
+    }
+
+    @Test
+    fun `Base time is set correctly`() { // TODO: rename ?
+        mainViewModel.toggleTimer(0)
+
+        mainViewModel.toggleTimer(6) // so 6s
+
+        mainViewModel.toggleTimer(10) // start again after 4 seconds (so we want base to be 4)
+
+        assertEquals(4, mainViewModel.timerBase().value!!)
+    }
+    
 }
