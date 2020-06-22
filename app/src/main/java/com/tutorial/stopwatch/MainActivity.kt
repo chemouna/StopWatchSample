cfbf9bb4da3f1bd3b54032e02835df3c77ce1efd
Mouna Cheikhna <cheikhnamouna@gmail.com>
2020-06-21 22:07:13 -0700

    Clean up.

--- app/src/main/java/com/tutorial/stopwatch/MainActivity.kt
+++ app/src/main/java/com/tutorial/stopwatch/MainActivity.kt
@@ -2,7 +2,7 @@ package com.tutorial.stopwatch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.PersistableBundle
+import android.util.Log
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import kotlinx.android.synthetic.main.activity_main.*
@@ -16,21 +16,22 @@ class MainActivity : AppCompatActivity() {
         val viewModel = ViewModelProvider(this)[MainViewModel::class.java]
         viewModel.timerState().observe(this, Observer { isRunning ->
             if (isRunning) {
-                timer.stop()
-                toggle_timer.text = getString(R.string.start)
-            } else {
                 timer.start()
                 toggle_timer.text = getString(R.string.stop)
+            } else {
+                timer.stop()
+                toggle_timer.text = getString(R.string.start)
             }
         })
 
-        viewModel.startTime().observe(this, Observer { time ->
+        viewModel.timerBase().observe(this, Observer { time ->
             timer.base = time
         })
 
         toggle_timer.setOnClickListener {
             viewModel.toggleTimer()
         }
+
         reset_timer.setOnClickListener {
             viewModel.resetTimer()
         }
